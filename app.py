ec2e78b3f26f553c5cc1ad90016fabcf96afb827
Dan Tenenbaum <dtenenba@fhcrc.org>
2017-08-31 08:29:01 -0700

    use names appropriate for constants

--- app.py
+++ app.py
@@ -2,9 +2,9 @@
 
 """
 A RESTful Flask app to add/remove users from
-a given GitHub organization (presumably FredHutch).
-Set the organization in the environment variable
-GITHUB_ORG. Set your github authentication token
+a given GitHub ORGanization (presumably FredHutch).
+Set the ORGanization in the environment variable
+GITHUB_ORG. Set your GITHUB authentication TOKEN
 in GITHUB_TOKEN.
 
 Pass a 'username' parameter to the '/' endpoint
@@ -12,14 +12,14 @@ for the following actions:
 
 HTTP verb    |   action
 ------------------------
-GET          |  determine if user is member of org
-DELETE       |  remove user from org
-PUT          |  add user to org
+GET          |  determine if user is member of ORG
+DELETE       |  remove user from ORG
+PUT          |  add user to ORG
 
 If app is running at http://localhost:5000/, the following
 command lines will work:
 
-# is user foo a member of the org?
+# is user foo a member of the ORG?
 curl -X GET -d username=foo http://localhost:5000/
 # returns {"status": "yes"}
 
@@ -27,7 +27,7 @@ curl -X GET -d username=foo http://localhost:5000/
 curl -X PUT -d username=foo http://localhost:5000/
 # returns {"status": "user has been invited"}
 
-# remove user foo from org
+# remove user foo from ORG
 curl -X DELETE -d username=foo http://localhost:5000/
 # returns {"status": "OK"}
 """
@@ -42,56 +42,56 @@ from flask import Flask
 from flask_restful import Resource, Api, reqparse
 
 
-app = Flask(__name__) # pylint: disable=invalid-name
-api = Api(app)  # pylint: disable=invalid-name
-github = Github('https://api.github.com')  # pylint: disable=invalid-name
+APP = Flask(__name__) # pylint: disable=invalid-name
+API = Api(APP)
+GITHUB = Github('https://API.GITHUB.com')
 
-token = os.getenv('GITHUB_TOKEN') # pylint: disable=invalid-name
-if not token:
+TOKEN = os.getenv('GITHUB_TOKEN')
+if not TOKEN:
     print("GITHUB_TOKEN not set!")
     sys.exit(1)
 
-headers = {} # pylint: disable=invalid-name
-headers["Authorization"] = "token {}".format(token)
-headers["Accept"] = "application/vnd.github.hellcat-preview+json"
+HEADERS = {}
+HEADERS["Authorization"] = "TOKEN {}".format(TOKEN)
+HEADERS["Accept"] = "application/vnd.GITHUB.hellcat-preview+json"
 
-org = os.getenv("GITHUB_ORG") # pylint: disable=invalid-name
-if not org:
+ORG = os.getenv("GITHUB_ORG")
+if not ORG:
     print("GITHUB_ORG not set!")
     sys.exit(1)
 
 class GithubTeamManager(Resource):
-    """RESTful class for managing org memberships"""
+    """RESTful class for managing ORG memberships"""
 
     def get(self): # pylint: disable=no-self-use
-        """GET method; query if user is member of org"""
+        """GET method; query if user is member of ORG"""
         parser = reqparse.RequestParser()
         parser.add_argument('username', type=str,
-                            help='github username to look up',
+                            help='GITHUB username to look up',
                             required=True)
         args = parser.parse_args()
-        result = github.orgs(org).members(args.username).GET(headers=headers)
+        result = GITHUB.ORGs(ORG).members(args.username).GET(HEADERS=HEADERS)
         if result.status_code == 204:
             value = "yes"
         elif result.status_code == 404:
             value = "no"
         elif result.status_code == 302:
-            value = "you are not an organization member"
+            value = "you are not an ORGanization member"
         return {'status': value}
 
     def put(self): # pylint: disable=no-self-use
         """
-        PUT method, invite user to org.
+        PUT method, invite user to ORG.
         They will be invited and their membership state marked
         as 'pending'. When they accept the invite
         their membership state will be changed to 'active'.
         """
         parser = reqparse.RequestParser()
         parser.add_argument('username', type=str,
-                            # help='github username to look up',
+                            # help='GITHUB username to look up',
                             required=True)
         args = parser.parse_args()
-        result = github.orgs(org).memberships(args.username).PUT(headers=headers)
+        result = GITHUB.ORGs(ORG).memberships(args.username).PUT(HEADERS=HEADERS)
         obj = result.json()
         if obj['state'] == 'pending':
             value = "user has been invited"
@@ -102,16 +102,17 @@ class GithubTeamManager(Resource):
         return {"status": value}
 
     def delete(self): # pylint: disable=no-self-use
-        """DELETE method, removes user from org."""
+        """DELETE method, removes user from ORG."""
         parser = reqparse.RequestParser()
         parser.add_argument('username', type=str,
-                            # help='github username to look up',
+                            # help='GITHUB username to look up',
                             required=True)
         args = parser.parse_args()
-        # first look up all teams in the org to see if the
+        # first look up all teams in the ORG to see if the
         # user is in any of those teams & needs to be removed from them.
-        org_teams = github.orgs(org).teams.GET(headers=headers)
-        result = github.orgs(org).members(args.username).DELETE(headers=headers)
+        org_teams_result = GITHUB.ORGs(ORG).teams.GET(HEADERS=HEADERS)
+
+        result = GITHUB.ORGs(ORG).members(args.username).DELETE(HEADERS=HEADERS)
         if result.status_code == 204:
             value = "OK"
         elif result.status_code == 404:
@@ -120,11 +121,11 @@ class GithubTeamManager(Resource):
             value = "ERROR"
         return {"status": value}
 
-api.add_resource(GithubTeamManager, '/')
+API.add_resource(GithubTeamManager, '/')
 
 # run me with:
-# FLASK_DEBUG=True FLASK_APP=app.py flask run
+# FLASK_DEBUG=True FLASK_APP=APP.py flask run
 # or simply:
 # python3 app.py
 if __name__ == '__main__':
-    app.run(debug=True)
+    APP.run(debug=True)
