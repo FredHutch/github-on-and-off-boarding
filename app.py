b35db48bd88ae0e19c3fd6409861d7999dd5dbed
Dan Tenenbaum <dtenenba@fhcrc.org>
2018-10-29 09:04:11 -0700

    let the github_team_syncer webhook handle removing from AllFredHutch team

--- app.py
+++ app.py
@@ -44,11 +44,11 @@ from flask import Flask, request
 from flask_restful import Resource, Api, reqparse
 
 
-APP = Flask(__name__) # pylint: disable=invalid-name
+APP = Flask(__name__)  # pylint: disable=invalid-name
 API = Api(APP)
-GITHUB = Github('https://api.github.com')
+GITHUB = Github("https://api.github.com")
 
-TOKEN = os.getenv('GITHUB_TOKEN')
+TOKEN = os.getenv("GITHUB_TOKEN")
 if not TOKEN:
     print("GITHUB_TOKEN not set!")
     sys.exit(1)
@@ -65,6 +65,7 @@ if not ORG:
 # localhost, jamborite, hamtramck, and gizmo1
 APPROVED_IPS = ["127.0.0.1", "140.107.42.44", "140.107.73.85", "140.107.134.232"]
 
+
 def get_remote_ip(req):
     """
     Get remote IP from request.
@@ -76,24 +77,25 @@ def get_remote_ip(req):
     so look for the comma and reject those connections.
     """
     if "X-Forwarded-For" in req.headers:
-        ip_ = req.headers['X-Forwarded-For']
+        ip_ = req.headers["X-Forwarded-For"]
         if "," in ip_:
             raise ValueError
         return ip_
     return req.remote_addr
+
 
 def get_paginated_results(url, delay=0.3):
     """
     Handle paginated results transparently, returning them as one list.
     """
     results = requests.get(url, headers=HEADERS)
-    if not 'Link' in results.headers:
+    if not "Link" in results.headers:
         return results.json()
     all_results = results.json()
     while True:
-        if not 'rel="next"' in results.headers['Link']:
+        if not 'rel="next"' in results.headers["Link"]:
             return all_results
-        links = results.headers['Link'].split(",")
+        links = results.headers["Link"].split(",")
         nextpage = [x for x in links if 'rel="next"' in x][0]
         nextpage = nextpage.split(";")[0].replace("<", "").replace(">", "").strip()
         time.sleep(delay)
@@ -106,12 +108,12 @@ def get_paginated_results(url, delay=0.3):
 class GithubOnOffBoarder(Resource):
     """RESTful class for managing org/team memberships"""
 
-    def get(self): # pylint: disable=no-self-use
+    def get(self):  # pylint: disable=no-self-use
         """GET method; query if user is member of ORG"""
         parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str,
-                            help='GITHUB username to look up',
-                            required=True)
+        parser.add_argument(
+            "username", type=str, help="GITHUB username to look up", required=True
+        )
         args = parser.parse_args()
         result = GITHUB.orgs(ORG).members(args.username).GET(headers=HEADERS)
         if result.status_code == 204:
@@ -124,9 +126,9 @@ class GithubOnOffBoarder(Resource):
             value = "not properly authorized"
         else:
             value = "unknown"
-        return {'status': value}
+        return {"status": value}
 
-    def put(self): # pylint: disable=no-self-use
+    def put(self):  # pylint: disable=no-self-use
         """
         PUT method, invite user to ORG.
         They will be invited and their membership state marked
@@ -135,33 +137,53 @@ class GithubOnOffBoarder(Resource):
         """
         remote_ip = get_remote_ip(request)
         if not remote_ip in APPROVED_IPS:
-            return dict(status="unknown ip {}, call me from jamborite or gizmo1".format(remote_ip)), 500
+            return (
+                dict(
+                    status="unknown ip {}, call me from jamborite or gizmo1".format(
+                        remote_ip
+                    )
+                ),
+                500,
+            )
         parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str,
-                            # help='GITHUB username to look up',
-                            required=True)
+        parser.add_argument(
+            "username",
+            type=str,
+            # help='GITHUB username to look up',
+            required=True,
+        )
         args = parser.parse_args()
         result = GITHUB.orgs(ORG).memberships(args.username).PUT(headers=HEADERS)
         obj = result.json()
-        if 'message' in obj:
+        if "message" in obj:
             return obj
-        if obj['state'] == 'pending':
+        if obj["state"] == "pending":
             value = True
-        elif obj['state'] == 'active':
+        elif obj["state"] == "active":
             value = "user is already a member"
         else:
             value = "ERROR"
         return {"status": value}
 
-    def delete(self): # pylint: disable=no-self-use
+    def delete(self):  # pylint: disable=no-self-use
         """DELETE method, removes user from ORG."""
         remote_ip = get_remote_ip(request)
         if not remote_ip in APPROVED_IPS:
-            return dict(status="unknown ip {}, call me from jamborite or gizmo1".format(remote_ip)), 500
+            return (
+                dict(
+                    status="unknown ip {}, call me from jamborite or gizmo1".format(
+                        remote_ip
+                    )
+                ),
+                500,
+            )
         parser = reqparse.RequestParser()
-        parser.add_argument('username', type=str,
-                            # help='GITHUB username to look up',
-                            required=True)
+        parser.add_argument(
+            "username",
+            type=str,
+            # help='GITHUB username to look up',
+            required=True,
+        )
         args = parser.parse_args()
         # first look up all teams in the org to see if the
         # user is in any of those teams & needs to be removed from them.
@@ -170,15 +192,29 @@ class GithubOnOffBoarder(Resource):
         # this will do a lot of I/O, but since this is not called very often,
         # it should not hit our rate limit.
         for team in teams:
-            result = GITHUB.teams(team['id']).memberships(args['username']).GET(headers=HEADERS)
+            result = (
+                GITHUB.teams(team["id"])
+                .memberships(args["username"])
+                .GET(headers=HEADERS)
+            )
             # if status code is 200, the user is a member of the team, whether pending,
             # active, or even a team maintainer.
             if result.status_code == 200:
                 on_teams.append(team)
 
+        # remove AllFredHutch from on_teams because the webhook attached to the org
+        # (https://github.com/FredHutch/github_team_syncer) is already handling
+        # adding/deleting people to/from that team.
+        on_teams = [x for x in on_teams if not x["name"] == "AllFredHutch"]
+
+        #
         # remove user from teams
         for team in on_teams:
-            result = GITHUB.teams(team['id']).memberships(args['username']).DELETE(headers=HEADERS)
+            result = (
+                GITHUB.teams(team["id"])
+                .memberships(args["username"])
+                .DELETE(headers=HEADERS)
+            )
 
         # now remove user from team
         result = GITHUB.orgs(ORG).members(args.username).DELETE(headers=HEADERS)
@@ -190,11 +226,12 @@ class GithubOnOffBoarder(Resource):
             value = "ERROR"
         return {"status": value}
 
-API.add_resource(GithubOnOffBoarder, '/')
+
+API.add_resource(GithubOnOffBoarder, "/")
 
 # run me with:
 # FLASK_DEBUG=True FLASK_APP=app.py flask run
 # or simply:
 # python3 app.py
-if __name__ == '__main__':
+if __name__ == "__main__":
     APP.run(debug=True)
